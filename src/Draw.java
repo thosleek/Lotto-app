59d04eafd75b1beaf33888fbdc91e48a99cb6333
Tom Gladkowski <nuubon@gmail.com>
2017-07-05 10:02:58 +0100

    v0.03

--- src/Draw.java
+++ src/Draw.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+
 public class Draw {
 
     public int drawId;
@@ -20,28 +26,57 @@ public class Draw {
         this.drawNumbers = drawNumbers;
     }
 
+
     public Draw(int drawId, int[] drawNumbers) {
         this.drawId = drawId;
         this.drawNumbers = drawNumbers;
     }
 
     public Draw(int[] drawNumbers) {
-
         this.drawNumbers = drawNumbers;
         this.drawId = this.newDrawId();
     }
 
+    /*public Draw(int drawId) {
+        this.drawId = drawId;
+        // TODO: za chuj nie dziala, good luck dude
+
+        try {
+            String fileName = Integer.toString(drawId) + ".txt";
+            Scanner loadDraw = new Scanner(new File(fileName)).useDelimiter(",\\s*");
+            List<Integer> temps = new ArrayList<Integer>();
+
+            while(loadDraw.hasNext()){
+                int token1 = loadDraw.nextInt();
+                temps.add(token1);
+            }
+            loadDraw.close();
+            this.drawNumbers = temps.toArray(new Integer[0]);
+
+        } catch (IOException e){
+            System.out.println("no file");
+        }
+    }*/
+
     public void display(){
-        System.out.println("Draw id: " + drawId);
-        System.out.print("Numbers drown:");
-        for(int number: drawNumbers){
+        System.out.println("Draw id: " + this.getDrawId());
+        System.out.print("Numbers drawn:");
+        for(int number: this.getDrawNumbers()){
             System.out.print(" " + number);
         }
     }
 
     public int newDrawId(){
-        int drawId = 101;
+        int drawId = 1000; // first draw ever
         //TODO: checks files, looks for highest no, returns new draw id
+       boolean gotDrawId = false;
+        while(!gotDrawId){
+            String fileName = "draws/" + Integer.toString(drawId) + ".txt";
+            File file = new File(fileName);
+            if(file.exists()){
+                drawId++;
+            } else gotDrawId = true;
+        }
         return drawId;
     }
 }
--- src/DrawMachine.java
+++ src/DrawMachine.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.io.File;
 
 public class DrawMachine {
     int quantity, range;
